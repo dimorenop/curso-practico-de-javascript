b51904c0f151d0dccd8c132b2e59e941e7dadb46
Diego Moreno <diamorenope@unal.edu.co>
2022-10-11 10:17:25 -0500

    sacamos los valores de los inputs e hicimos una suma

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
 const h1 = document.querySelector("h1");
-const p = document.querySelector("p");
-const parrafito = document.querySelector(".parrafito");
-const pid = document.querySelector("#pid");
-const input = document.querySelector("input");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
 
-console.log({
-  h1,
-  p,
-  parrafito,
-  pid,
-  input,
-});
-
-h1.innerHTML = "patito <br> Feo";
-h1.innerText = "patito <br> Feo";
-// console.log(h1.getAttribute("class"))
-// h1.setAttribute("class", "rojo")
-h1.classList.add("rojo");
-h1.classList.remove("verde");
-// h1.classList.toggle("verde");
-// h1.classList.contains("verde");
-
-input.placeholder = "hola";
-
-const img = document.createElement("img");
-img.setAttribute(
-  "src",
-  "https://johnpapa.gallerycdn.vsassets.io/extensions/johnpapa/vscode-peacock/4.2.2/1663810940966/Microsoft.VisualStudio.Services.Icons.Default"
-);
-
-pid.innerHTML = "";
-pid.append(img);
+function btnOnClick() {
+    let resultado = Number(input1.value) + Number(input2.value);
+    pResult.innerText = `La suma es ${resultado}`
+    console.log(`La suma es ${resultado}`)
+}
