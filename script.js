15c8c80a9979457dc2e6fc0b77fd657fa877b4ef
Diego Moreno <diamorenope@unal.edu.co>
2022-10-11 09:23:28 -0500

    modificación de HTML desde JS

--- script.js
+++ script.js
@@ -5,9 +5,29 @@ const pid = document.querySelector("#pid");
 const input = document.querySelector("input");
 
 console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input
-})
+  h1,
+  p,
+  parrafito,
+  pid,
+  input,
+});
+
+h1.innerHTML = "patito <br> Feo";
+h1.innerText = "patito <br> Feo";
+// console.log(h1.getAttribute("class"))
+// h1.setAttribute("class", "rojo")
+h1.classList.add("rojo");
+h1.classList.remove("verde");
+// h1.classList.toggle("verde");
+// h1.classList.contains("verde");
+
+input.placeholder = "hola";
+
+const img = document.createElement("img");
+img.setAttribute(
+  "src",
+  "https://johnpapa.gallerycdn.vsassets.io/extensions/johnpapa/vscode-peacock/4.2.2/1663810940966/Microsoft.VisualStudio.Services.Icons.Default"
+);
+
+pid.innerHTML = "";
+pid.append(img);
