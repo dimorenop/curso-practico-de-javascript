858a3fb9f2f9f9cc53e84ef97554317444bf76fe
Diego Moreno <diamorenope@unal.edu.co>
2022-10-11 09:03:48 -0500

    Hola, mundo

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const h1 = document.querySelector("h1");
-const p = document.querySelector("p");
-const parrafito = document.querySelector(".parrafito");
-const pid = document.querySelector("#pid");
-const input = document.querySelector("input");
-
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input
-})
+console.log('Hello, world')
