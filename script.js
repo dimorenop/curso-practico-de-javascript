c4e04eb7516de1a30f4d87e46c8fc46049c8540b
Diego Moreno <diamorenope@unal.edu.co>
2022-10-18 17:53:06 -0500

    uso de add event listener

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const h1 = document.querySelector("h1");
+const form = document.querySelector("#form");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
 const btn = document.querySelector("#btnCalcular");
 const pResult = document.querySelector("#result");
+    
+btn.addEventListener('click', sumarInputValues)
 
-function btnOnClick() {
+function sumarInputValues(event) {
     let resultado = Number(input1.value) + Number(input2.value);
     pResult.innerText = `La suma es ${resultado}`
     console.log(`La suma es ${resultado}`)
 }
+
+/* form.addEventListener('submit', sumarInputValues)
+
+
+function sumarInputValues(event) {
+    console.log({event})
+    event.preventDefault();
+    let resultado = Number(input1.value) + Number(input2.value);
+    pResult.innerText = `La suma es ${resultado}`
+    console.log(`La suma es ${resultado}`)
+} */
+
